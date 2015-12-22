7d9c61db0ad4a556b1cd2d69cd86d331d0f5462b
N. Justus <nicola.justus@mni.thm.de>
2015-12-22 15:08:09 +0100

    Added save-playlist-button. Fixed NotFound-Ressource errors.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   $('tr.playlist-item').on('dblclick', playSong);
   $('td span#remove-song').on('click', removeSong);
   $('tr.playlist-item').on('click', markSong);
+  //$('td span#save-playlist-btn').on('click')
 
   // ==================== playing line
   $('span#volume-minus-btn').on('click', function() {
