cb5bcff02797eaf6563f4d65c40be56718384f27
N. Justus <nicola.justus@mni.thm.de>
2015-12-27 16:10:44 +0100

    Added todo comment

--- app/Global.scala
+++ app/Global.scala
@@ -5,6 +5,11 @@ import play.api.{ GlobalSettings, Logger }
 object Global extends GlobalSettings {
 
   override def onStart(app: play.api.Application): Unit = {
+    /*TODO
+      !!! important !!!
+      retry sending connect until no NullPointerException occurs or
+      no of retrys exceeds
+    */
     val mpdConnector = MpdConnector.getMpdActor
     mpdConnector ! models.mpdbackend.Connect
     Logger.info("Application has started")
