7deee50f554c49bbafcc257bc6ae14e515b8655f
N. Justus <nicola.justus@mni.thm.de>
2015-12-30 17:29:27 +0100

    Added side for streaming the music over mpd's integrated webserver

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import scala.concurrent.Future
 import models.mpdbackend.MpdConnector
 import akka.actor.ActorRef
 import models.mpdbackend._
+import play.api.Play
 
 class Application extends Controller {
   def index = playlist
@@ -63,44 +64,16 @@ class Application extends Controller {
     }
   }
 
-  /*def lib(artist:Option[String], album:Option[String]) = Action {
-    val library = List(
-      "Linkin Park" -> "Meteora",
-      "Linkin Park" -> "Minutes to midnight",
-      "Paramore" -> "Live 2003",
-      "Paramore" -> "Self-titled",
-      "Blink182" -> "Greatest Hits",
-      "Blink182" -> "Enema of the state",
-      "Blink182" -> "Take of your pants and jackets"
-    )
-
-    println(artist, album)
-
-    (artist, album) match {
-      case (Some(art), Some(alb)) =>
-        val filteredList = library.filter( x => x._1 == art && x._2 == alb ).map(_._2).sorted
-        val artistList = views.html.templates.lib_list(filteredList, s"$art - $alb") { x =>
-          controllers.routes.Application.lib(None, None)
-        }
-        Ok(views.html.lib(artistList))
-      case (Some(art), None) =>
-        val filteredList = library.filter( x => x._1 == art ).map(_._2).sorted
-        val artistList = views.html.templates.lib_list(filteredList, art) { x =>
-          controllers.routes.Application.lib(Some(art), Some(x))
-        }
-        Ok(views.html.lib(artistList))
-      case _ =>
-        val filteredList = library.map(_._1).sorted
-        val artistList = views.html.templates.lib_list(filteredList, "Artist") { x =>
-          controllers.routes.Application.lib(Some(x), None)
-        }
-        Ok(views.html.lib(artistList))
+  def stream = Action.async {
+    val mpdConnector = getMpdActor
+    getPlayerStatus(mpdConnector) { implicit status =>
+      Future {
+        val adrOpt = Play.current.configuration.getString("mpd.streaming.ip")
+        val ipOpt = Play.current.configuration.getString("mpd.streaming.port")
+        Ok(views.html.stream_music(adrOpt, ipOpt))
+      }
     }
   }
-
-  def about = Action {
-    Ok(views.html.about())
-  }*/
 
   def about = Action.async {
     val mpdConnector = getMpdActor
