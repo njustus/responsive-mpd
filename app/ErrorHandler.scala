23a976278799331c2631671c6d24bd18b4b7c1dc
N. Justus <nicola.justus@mni.thm.de>
2015-12-30 16:59:26 +0100

    Added error-handler for production server. Added error-page Removed deprecated config-keys

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,62 @@
+import javax.inject._
+import play.api.http.DefaultHttpErrorHandler
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.routing.Router
+import scala.concurrent._
+import org.bff.javampd.exception._
+import java.net.NoRouteToHostException
+import scala.annotation.tailrec
+
+class ErrorHandler @Inject() (
+    env: Environment,
+    config: Configuration,
+    sourceMapper: OptionalSourceMapper,
+    router: Provider[Router]
+  ) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) {
+
+  override def onDevServerError(request: RequestHeader, exception: UsefulException) =
+    onProdServerError(request, exception)
+
+  override def onProdServerError(request: RequestHeader, exception: UsefulException) =
+    Future {
+      exceptionHandler(exception.cause)
+  }
+
+  private def exceptionHandler: PartialFunction[Throwable, Result] =
+      mpdException.orElse(otherExceptions).andThen { msg => InternalServerError(views.html.error(msg)) }
+
+  @tailrec
+  private def getInnerstException(t:Throwable): Throwable = {
+    if(t.getCause != null) getInnerstException(t.getCause)
+    else t
+  }
+
+  private def mpdException: PartialFunction[Throwable, String] =
+    getInnerstException(_) match {
+      case exc:NoRouteToHostException => "Can't connect to the server:\n" + exc.getMessage
+      case exc:MPDPlayerException =>
+        "Player exception: " + lastException(exc.getMessage)
+      case exc:MPDTimeoutException => "Timeout exc"
+      case exc:MPDConnectionException => "An error occured while connecting to the mpd server:\n " + exc.getMessage
+      case exc:MPDException => "An unknown error from mpd occured:\n" + exc.getMessage
+    }
+
+  private def otherExceptions: PartialFunction[Throwable, String] = {
+    case null => "An unknown exception occured!"
+    case exc:Throwable => "System totally corrupted!\nPlease consult the developers and report this message:\n" + exc.toString()
+  }
+
+  private def lastException(msg:String): String = {
+    val tmp = msg.split(":")
+    tmp(tmp.length-1)
+  }
+
+  /*override def onForbidden(request: RequestHeader, message: String) = {
+    Future.successful(
+      Forbidden("You're not allowed to access this resource.")
+    )
+  }*/
+}
