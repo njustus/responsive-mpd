0c1a74d19f59b31760bd9ab35cfee10582de6e75
N. Justus <nicola.justus@mni.thm.de>
2015-12-31 23:34:40 +0100

    Added closure over client's-websocket

--- public/javascripts/clientsocket.js
+++ public/javascripts/clientsocket.js
@@ -1,8 +1,17 @@
-clientSocket = new WebSocket("ws://" + globalUris.websocket);
+function newSocketHandler() {
+  clientSocket = new WebSocket("ws://" + globalUris.websocket);
 
-clientSocket.onopen = function(evt) {
-  clientSocket.send("first msg");
-}
-clientSocket.onmessage = function(msg) {
-  console.log("Got msg: "+msg):
+  clientSocket.onopen = function(evt) {
+    clientSocket.send("first msg");
+  }
+  clientSocket.onmessage = function(msg) {
+    console.log("Got msg: "+msg):
+  }
+
+  var handler = {
+    send: function(obj) {
+      clientSocket.send(JSON.stringify(obj));
+    }
+  };
+  return handler;
 }
