53fb638138888298ef9ea93aa44ebea6347423ab
N. Justus <nicola.justus@mni.thm.de>
2016-02-03 20:39:32 +0100

    Added toJson-function for JsPlaySong. Removed unused methods & stuff.

--- app/models/JsMessages.scala
+++ app/models/JsMessages.scala
@@ -1,10 +1,15 @@
 package models
 
-import play.api.data.validation.ValidationError
-import play.api.libs.json._ // JSON library
-import play.api.libs.json.Reads._ // Custom validation helpers
-import play.api.libs.functional.syntax._ // Combinator syntax
+import org.bff.javampd.objects.MPDSong
 
+import play.api.data.validation.ValidationError
+import play.api.libs.json.{ JsValue, Json }
+
+/**
+ * JsMessages are '''only''' used for '''server-to-client communication''' over websockets.
+ *
+ * NOT for client-to-server, client-to-server should communicate over the RESTfull-API.
+ */
 object JsMessages {
 
   sealed abstract class JsAction(actionId:Int) {
@@ -22,27 +27,15 @@ object JsMessages {
   case object JsStop extends JsAction(1)
   case object JsNext extends JsAction(2)
   case object JsPrev extends JsAction(3)
-  case class  JsPlayId(id:Int) extends JsAction(4) {
-    override protected def internalJson: String = s""" "id":$id """
+  case class  JsPlaySong(s:MPDSong) extends JsAction(4) {
+    override protected def internalJson: String = s"""
+      "position": ${s.getPosition},
+      "title":${s.getTitle},
+      "artist":${s.getArtistName},
+      "album":${s.getAlbumName}
+      """
   }
+  case object JsReloadPage extends JsAction(5)
 
   private val parsingError = ValidationError("Can't prove that given json is a JsAction!")
-
-  private val readActionId: Reads[Int] = (JsPath \ "action-id").read[Int]
-  val objReads: Reads[JsAction] = readActionId.collect(parsingError)(getMsgByObjectsId)
-
-  val playId: Reads[JsAction] = (
-    readActionId and
-    (JsPath \ "id").read[Int] ).tupled.collect(parsingError) {
-    case (4, id) => JsPlayId(id)
-  }
-
-  val reads: Reads[JsAction] = playId.orElse(objReads)
-
-  def getMsgByObjectsId: PartialFunction[Int, JsAction] = {
-    case 0 => JsPlay
-    case 1 => JsStop
-    case 2 => JsNext
-    case 3 => JsPrev
-  }
 }
