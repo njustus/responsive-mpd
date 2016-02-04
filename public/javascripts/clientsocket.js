c2d1d34a025305e4954f57e81e1606feb9ebcc12
N. Justus <nicola.justus@mni.thm.de>
2016-02-04 17:51:00 +0100

    Implemented handling for song has changed.

--- public/javascripts/clientsocket.js
+++ public/javascripts/clientsocket.js
@@ -5,21 +5,23 @@ function newSocketHandler() {
     console.log("connection opened");
   }
   clientSocket.onmessage = function(msg) {
-    console.log(msg.data);
-	var data = JSON.parse(msg.data);
-	console.log(data);
-      switch(data.action_id) {
-      	case 0: case 1:
-      		console.log("id 0,1: play toggle");
-      		playerHandler.playToggle(true);
-      		break;
-      	case 2: break;
-      	case 3: break;
-      	case 4: break;
-      	case 5:
-      		clientSocket.close();
-      		location.reload(true);
-      		break;
+	  var data = JSON.parse(msg.data);
+    switch(data.action_id) {
+    	case 0: case 1:
+    		console.log("play toggle");
+    		playerHandler.playToggle(true);
+    		break;
+    	case 2: break;
+    	case 3: break;
+    	case 4:
+        var elem = $('li.actual-song-text > span#actual-song-line');
+        var newLine = data.artist + " - " + data.title;
+        elem.html(newLine);
+        break;
+    	case 5:
+    		clientSocket.close();
+    		location.reload(true);
+    		break;
       }
   }
 
@@ -31,10 +33,10 @@ function newSocketHandler() {
   		clientSocket.close();
   	}
   };
-  
+
   window.onbeforeunload = function() {
 	 clientSocket.close();
   }
-  
+
   return handler;
 }
