bb3d1d22fefbe968bb7a40137ef252cc14b7c3ae
N. Justus <nicola.justus@mni.thm.de>
2016-02-13 16:13:33 +0100

    Started implementing own (actor-based) mpd-monitor.

--- app/models/mpdbackend/MpdMaster.scala
+++ app/models/mpdbackend/MpdMaster.scala
@@ -2,10 +2,21 @@ package models.mpdbackend
 
 import akka.actor.{ Actor, Props, Terminated }
 import akka.routing.{ ActorRefRoutee, RoundRobinRoutingLogic, Router }
-
 import play.api.Logger
+import play.api.Play
+import play.api.Play.current
+import play.api.libs.concurrent.Akka
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import akka.actor.PoisonPill
 
 class MpdMaster extends Actor with MpdSupervisor {
+
+  private val lookupInterval = playConf.getInt("mpd.lookup-interval").getOrElse(5) seconds
+
+  private val monitorActor = context.actorOf( Props(classOf[MpdMonitoringActor], MpdConnector.getMpdActor), "monitoringActor")
+  private val scheduledTask = Akka.system.scheduler.schedule(10 milliseconds, lookupInterval, monitorActor, MpdMonitoringActor.Lookup)
 
   private var router = {
     val routes = Vector.fill(sizeOfPool) {
@@ -17,7 +28,14 @@ class MpdMaster extends Actor with MpdSupervisor {
     Router(RoundRobinRoutingLogic(), routes)
   }
 
+  override def postStop(): Unit = {
+    scheduledTask.cancel()
+    monitorActor ! PoisonPill
+  }
+
   def receive = {
+    case AddSocketListener => monitorActor forward AddSocketListener
+    case RemoveSocketListener => monitorActor forward RemoveSocketListener
     case msg:ConnectorMessage => router.route(msg, sender())
     case Terminated(actor) =>
       //one actor terminated, start new one
