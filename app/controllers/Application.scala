ef716999c26aac065f4a0e9547721067a7f584e2
N. Justus <nicola.justus@mni.thm.de>
2016-02-13 17:01:53 +0100

    Removed done todos. Added 1 more todo.. ;)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,8 +15,7 @@ class Application extends AbstractMpdController {
   def index = Action { Redirect(routes.Application.playlist()) }
 
   /*
-   * TODO add socket-event for repeating/shuffling: implement own actor-based listener that uses custom messages, there are no events for shuffling, repeating, etc..
-   * TODO move list of listener-actors to mpdmaster: if connector terminated all listeners are lost
+   * TODO Change return of GetActualSong to Option[Song] .. this song could be null (if playlist is empty), which breaks akka's message-delivery roules
    * TODO split up connectors behaviour in several traits, mixin in the behaviour and putting the partialfns together
    * TODO add tooltip-texts to savePlaylist, clearPlaylist, deleteSong, shuffle, repeat, + icons in lib/search
    */
