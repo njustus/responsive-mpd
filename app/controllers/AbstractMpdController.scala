970acea81a7b6949c0fb9c7405021c6ed6a1f3d1
N. Justus <nicola.justus@mni.thm.de>
2016-02-15 19:34:37 +0100

    Moved several methods from package.scala to AbstractMpdCtrl.scala

--- app/controllers/AbstractMpdController.scala
+++ app/controllers/AbstractMpdController.scala
@@ -1,17 +1,29 @@
 package controllers
 
 import akka.actor.ActorRef
-
 import scala.concurrent.Future
-
 import models.mpdbackend.MpdConnector
 import play.api.mvc._
+import models.mpdbackend.ConnectorMessage
 
 private[controllers] abstract trait AbstractMpdController extends Controller {
-  implicit val ctrl: Controller = this
 
   /* Defines a custom action which gets the RequestHeader & MpdConnector */
   def mpdAction(fn : Request[AnyContent] => ActorRef => Future[Result]) = Action.async { request =>
     val mpdConnector = MpdConnector.getMpdActor
     fn(request)(mpdConnector)
-  }}
+  }
+
+  def withActorMsg(msg:ConnectorMessage)
+    (fn: => Result) : Action[AnyContent] = Action { implicit request =>
+      val mpdActor = MpdConnector.getMpdActor
+      mpdActor ! msg
+      fn
+  }
+
+  def sendToActor(msg:ConnectorMessage) : Action[AnyContent] = Action { request =>
+      val mpdActor = MpdConnector.getMpdActor
+      mpdActor ! msg
+      Ok(msg.toString())
+  }
+}
--- app/controllers/package.scala
+++ app/controllers/package.scala
@@ -1,35 +1,16 @@
+import akka.actor.ActorRef
+import akka.pattern.ask
+
+import scala.concurrent.Future
+
+import models.MpdStatus
+import models.mpdbackend.MpdConnector.actorTimeout
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 package object controllers {
-  import akka.actor.actorRef2Scala
-  import akka.actor.ActorRef
-  import akka.pattern.ask
-  import models.{ MpdStatus, Title }
-  import models.mpdbackend.ConnectorMessage
-  import models.mpdbackend.MpdConnector
-  import models.mpdbackend.MpdConnector._
-  import scala.concurrent.Future
-  import play.api.libs.concurrent.Execution.Implicits.defaultContext
-  import play.api.mvc.{ Action, AnyContent, Controller, Result }
 
   private[controllers] def getPlayerStatus[T](mpdConnector:ActorRef)
     (fn: MpdStatus => Future[T]): Future[T] = {
-      (mpdConnector ? models.mpdbackend.GetMpdStatus)
-        .mapTo[MpdStatus].flatMap(fn)
+      (mpdConnector ? models.mpdbackend.GetMpdStatus).mapTo[MpdStatus].flatMap(fn)
     }
-
-  //TODO move into abstract controller
-  private[controllers] def withActorMsg(msg:ConnectorMessage)
-    (fn: => Result)
-    (implicit c:Controller) : Action[AnyContent] = Action { implicit request =>
-      val mpdActor = MpdConnector.getMpdActor
-      mpdActor ! msg
-      fn
-  }
-
-  //TODO move into abstract controller
-  private[controllers] def sendToActor(msg:ConnectorMessage)(implicit c:Controller) : Action[AnyContent] = Action {
-    implicit request =>
-      val mpdActor = MpdConnector.getMpdActor
-      mpdActor ! msg
-      c.Ok(msg.toString())
-  }
 }
