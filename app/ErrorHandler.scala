ff7cf5804336d35b62dfc666ade55cbcb9af41a0
N. Justus <nicola.justus@mni.thm.de>
2016-06-19 11:17:19 +0200

    Add :copyright: headers

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2015, 2016 Nicola Justus <nicola.justus@mni.thm.de>
+ * 
+ * This file is part of Responsive mpd.
+ * 
+ * Responsive mpd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Responsive mpd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Responsive mpd.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import scala.annotation.tailrec
 import scala.concurrent.Future
 
